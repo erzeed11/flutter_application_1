cf37d82f383f708b1cc9d0d75839fcd839d31cd9
Apis.nr <hafidznavidhar@gmail.com>
2023-07-12 10:27:33 +0700

    first commit

---
+++ lib/webview.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class View extends StatefulWidget {
+  const View({Key? key}) : super(key: key);
+
+  @override
+  State<View> createState() => _ViewState();
+}
+
+class _ViewState extends State<View> {
+  WebViewController? _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {},
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://getaplikasi.id/'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebViewWidget(controller: _controller!),
+    );
+  }
+}
